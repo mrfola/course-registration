47e0d4c111e3d6f8c3fb59b536c2a5bdc7d08f6d
Folaranmi Jesutofunmi <folaranmijesutofunmi@gmail.com>
2022-01-31 09:47:02 +0100

    Connected to live DB

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=course_registration',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_e01d35d715d03a7',
+    'username' => 'b57d4d538acc58',
+    'password' => 'db10603e',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
